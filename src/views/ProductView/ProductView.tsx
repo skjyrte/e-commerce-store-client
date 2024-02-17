f742334bf56a7fc7eca4bf94b6a6077d0d7fce09
Krzysztof <wasylewiczkrzysztof@gmail.com>
2024-02-17 11:28:29 +0100

    semicolon typo in productView removed

--- src/views/ProductView/ProductView.tsx
+++ src/views/ProductView/ProductView.tsx
@@ -148,7 +148,7 @@ const ProductView: FC<Props> = ({}) => {
           {renderGallery()}
         </div>
       </PortalModal>
-      {renderDescription()};
+      {renderDescription()}
     </div>
   );
 };
