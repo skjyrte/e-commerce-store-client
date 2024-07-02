62c8d3d33ea9455e08c53179351d409e30f575af
skjyrte <wasylewiczkrzysztof@gmail.com>
2024-07-02 19:24:05 +0200

    feat(api): implemented initial API requests for sending requests of gender-based products
    All the code is draft quality for preview.

--- src/views/CategoryView/CategoryView.tsx
+++ src/views/CategoryView/CategoryView.tsx
@@ -1,12 +1,116 @@
 import {FC, useEffect} from "react";
+import {Link, useLocation} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {AxiosResponse, AxiosRequestConfig} from "axios";
+/* custom components */
 import css from "./CategoryView.module.scss";
 import CategoryProductThumbnail from "../../components/thumbnails/CategoryProductThumbnail";
-import {Link} from "react-router-dom";
-import {useLocation} from "react-router-dom";
-import {useDispatch, useSelector} from "react-redux";
 import {AppDispatch} from "../../redux/configureStore";
 import {selectProductsByCategory} from "../../redux/selectors";
 import {switchGender} from "../../redux/slices/selectedGenderSlice";
+import createAxiosInstance from "../../api/createAxiosInstance";
+
+/* api */
+
+const axiosInstance = createAxiosInstance();
+
+interface ProductTemp {
+  id: string;
+  brand: string;
+  model: string;
+  gender: string;
+  category: string;
+  material: string;
+  season: string;
+  shortDescription: string;
+  description: string;
+  features: string[];
+  price: number;
+  initialPrice: number;
+  ratingReviews: number;
+  ratingValue: number;
+  thumbnail: string;
+  color: string;
+}
+
+interface ProductWithStockAndImages extends ProductTemp {
+  size?: string;
+  count?: number;
+  image_url?: string;
+}
+
+interface ApiResponse {
+  success: boolean;
+  message: string;
+  data?: ProductWithStockAndImages[];
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function getAxiosWrapper<T, R = AxiosResponse<T>, D = any>(
+  url: string,
+  data: Record<string, never>,
+  config: AxiosRequestConfig<D>
+) {
+  return axiosInstance.get<T, R, D>(url, config);
+}
+// eslint-disable-next-line
+async function handleRequest<T, D = any>(
+  requestPromise: (
+    // eslint-disable-next-line no-unused-vars
+    url: string,
+    // eslint-disable-next-line no-unused-vars
+    data: D,
+    // eslint-disable-next-line no-unused-vars
+    config: AxiosRequestConfig<D>
+  ) => Promise<AxiosResponse<ApiResponse>>,
+  requestConfig: {url: string; data: D; config: AxiosRequestConfig<D>},
+  // eslint-disable-next-line no-unused-vars
+  successCallback?: (data: ApiResponse) => void,
+  // eslint-disable-next-line no-unused-vars
+  failureCallback?: (error: unknown) => void
+): Promise<void> {
+  try {
+    const response = await requestPromise(
+      requestConfig.url,
+      requestConfig.data,
+      requestConfig.config
+    );
+
+    if (successCallback) {
+      if (typeof response === "object" && "data" in response) {
+        successCallback(response.data);
+      }
+    }
+  } catch (err) {
+    console.log(err);
+    if (failureCallback) {
+      failureCallback(err);
+    }
+  }
+}
+
+async function handleGetProducts(gender: string) {
+  handleRequest<ApiResponse>(
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    getAxiosWrapper<any>,
+    {
+      url: `/gender/${gender}`,
+      data: {},
+      config: {
+        params: {},
+      },
+    },
+    (data) => {
+      console.log("test data:");
+      console.log(data);
+    },
+    () => {
+      console.log("error cb");
+    }
+  );
+}
+
+/* component */
 
 const CategoryView: FC = () => {
   const location = useLocation();
@@ -23,9 +127,13 @@ const CategoryView: FC = () => {
   useEffect(() => {
     if (location.pathname === "/men") {
       dispatch(switchGender("men"));
+      /* PRINTS MEN */
+      handleGetProducts("men");
     }
     if (location.pathname === "/women") {
       dispatch(switchGender("women"));
+      /* PRINTS WOMEN */
+      handleGetProducts("women");
     }
   }, [location]);
 
