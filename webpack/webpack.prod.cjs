7432de53fcf271e2d350e5cc007ccca83798da19
skjyrte <wasylewiczkrzysztof@gmail.com>
2024-08-06 20:05:17 +0200

    fix: remove env loader for prod

--- webpack/webpack.prod.cjs
+++ webpack/webpack.prod.cjs
@@ -1,13 +1,7 @@
 const webpack = require("webpack");
-const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   mode: "production",
   devtool: "source-map",
-  plugins: [
-    new webpack.DefinePlugin({
-      /*       "process.env.fullClassName": JSON.stringify("[hash:base64]"), */
-    }),
-    new Dotenv({path: "./.env.prod"}),
-  ],
+  plugins: [new webpack.DefinePlugin({})],
 };
