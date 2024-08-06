12ee610397a1b8f58d60ead7540919961ce199b7
skjyrte <wasylewiczkrzysztof@gmail.com>
2024-08-06 20:41:37 +0200

    fix: env injection test

--- webpack/webpack.prod.cjs
+++ webpack/webpack.prod.cjs
@@ -3,5 +3,11 @@ const webpack = require("webpack");
 module.exports = {
   mode: "production",
   devtool: "source-map",
-  plugins: [new webpack.DefinePlugin({})],
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env.REACT_APP_API_URL": JSON.stringify(
+        process.env.REACT_APP_API_URL
+      ),
+    }),
+  ],
 };
