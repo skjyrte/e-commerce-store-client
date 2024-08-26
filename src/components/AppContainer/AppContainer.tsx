f3d89d929d1681543de353159fcfef2711df44b3
skjyrte <wasylewiczkrzysztof@gmail.com>
2024-08-26 19:39:49 +0200

    feat: add views transition

--- src/components/AppContainer/AppContainer.tsx
+++ src/components/AppContainer/AppContainer.tsx
@@ -1,7 +1,7 @@
 import {FC} from "react";
-import css from "./AppContainer.module.scss";
-import MainHeader from "../MainHeader";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {AnimatePresence} from "framer-motion";
+import MainHeader from "../MainHeader";
 import ProductView from "../../views/ProductView";
 import CategoryView from "../../views/CategoryView";
 import InvalidPageView from "../../views/InvalidPageView";
@@ -9,25 +9,93 @@ import CartView from "../../views/CartView";
 import HomeView from "../../views/HomeView";
 import LoginView from "../../views/LoginView";
 import RegisterView from "../../views/RegisterView";
+import PageTransition from "../PageTransition";
+import css from "./AppContainer.module.scss";
 
 const AppContainer: FC = () => {
   return (
     <BrowserRouter>
       <div className={css["app-container"]}>
         <MainHeader />
-        <Routes>
-          <Route path="/" element={<HomeView />} />
-          <Route path="/home" element={<CategoryView />} />
-          <Route path="/cart" element={<CartView />} />
-          <Route path="/login" element={<LoginView />} />
-          <Route path="/register" element={<RegisterView />} />
-          <Route path="/product/:id" element={<ProductView />} />
-          <Route path="/:gender" element={<CategoryView />} />
-          <Route path="/:gender/:category" element={<CategoryView />} />
-          <Route path="/*" element={<InvalidPageView />} />
-        </Routes>
+        <AnimatePresence mode="wait">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <PageTransition>
+                  <HomeView />
+                </PageTransition>
+              }
+            />
+            <Route
+              path="/home"
+              element={
+                <PageTransition>
+                  <CategoryView />
+                </PageTransition>
+              }
+            />
+            <Route
+              path="/cart"
+              element={
+                <PageTransition>
+                  <CartView />
+                </PageTransition>
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <PageTransition>
+                  <LoginView />
+                </PageTransition>
+              }
+            />
+            <Route
+              path="/register"
+              element={
+                <PageTransition>
+                  <RegisterView />
+                </PageTransition>
+              }
+            />
+            <Route
+              path="/product/:id"
+              element={
+                <PageTransition>
+                  <ProductView />
+                </PageTransition>
+              }
+            />
+            <Route
+              path="/:gender"
+              element={
+                <PageTransition>
+                  <CategoryView />
+                </PageTransition>
+              }
+            />
+            <Route
+              path="/:gender/:category"
+              element={
+                <PageTransition>
+                  <CategoryView />
+                </PageTransition>
+              }
+            />
+            <Route
+              path="/*"
+              element={
+                <PageTransition>
+                  <InvalidPageView />
+                </PageTransition>
+              }
+            />
+          </Routes>
+        </AnimatePresence>
       </div>
     </BrowserRouter>
   );
 };
+
 export default AppContainer;
---
+++ src/components/PageTransition/PageTransition.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import {motion} from "framer-motion";
+import {useLocation} from "react-router-dom";
+
+interface PageTransitionProps {
+  children: React.ReactNode;
+}
+
+const pageVariants = {
+  initial: {opacity: 0, x: 0},
+  in: {opacity: 1, x: 0},
+  out: {opacity: 0, x: 0},
+};
+
+const pageTransition = {
+  type: "tween",
+  ease: "anticipate",
+  duration: 0.5,
+};
+
+const PageTransition: React.FC<PageTransitionProps> = ({children}) => {
+  const thisLocation = useLocation();
+
+  return (
+    <motion.div
+      key={thisLocation.pathname}
+      initial="initial"
+      animate="in"
+      exit="out"
+      variants={pageVariants}
+      transition={pageTransition}
+      style={{width: "100%", height: "100%"}}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default PageTransition;
